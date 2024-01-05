8a47b515ab5e041e17211a8aba6e89f3618ea807
unknown <karolinadavidovich11@gmail.com>
2024-01-05 23:39:21 +0300

    added the solution of dz1

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите количество предметов:");
         Scanner scanner = new Scanner(System.in);
         int numberOfSubs = scanner.nextInt();
-        int summa=0;
+        int summa = 0;
         for (int i = 1; i < numberOfSubs + 1; i++) {
             System.out.println("Введите оценку за предмет " + i);
-            Scanner scanner1 = new Scanner(System.in);
-            int grade = scanner1.nextInt();
+            int grade = scanner.nextInt();
             if (grade > 10) {
-                System.out.println("Ошибка! Введите оценку от 1 до 10. Введите оценку еще раз");
-                int grade2 = scanner1.nextInt();
-                grade = grade2;
+                System.out.println("Ошибка! Введите оценку от 1 до 10.");
+                return;
             }
-        summa= summa + grade;
+            summa = summa + grade;
         }
-        int averageGrade = (summa) / numberOfSubs;
+        double averageGrade = (double) summa / numberOfSubs;
         System.out.println("Средняя оценка: " + averageGrade);
     }
 }
