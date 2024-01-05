52360ed5a56bba0aadf4c7250dfd152fbc1ecb3d
unknown <karolinadavidovich11@gmail.com>
2024-01-05 19:04:27 +0300

    added the solution of dz1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Введите количество предметов:");
+        Scanner scanner = new Scanner(System.in);
+        int numberOfSubs = scanner.nextInt();
+        int summa=0;
+        for (int i = 1; i < numberOfSubs + 1; i++) {
+            System.out.println("Введите оценку за предмет " + i);
+            Scanner scanner1 = new Scanner(System.in);
+            int grade = scanner1.nextInt();
+            if (grade > 10) {
+                System.out.println("Ошибка! Введите оценку от 1 до 10. Введите оценку еще раз");
+                int grade2 = scanner1.nextInt();
+                grade = grade2;
+            }
+        summa= summa + grade;
+        }
+        int averageGrade = (summa) / numberOfSubs;
+        System.out.println("Средняя оценка: " + averageGrade);
     }
 }
